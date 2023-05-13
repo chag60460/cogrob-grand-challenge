ecb916e90b0716202b408e88b50e19941c8674da
Grace <gc1@mit.edu>
2023-05-13 15:35:55 -0400

    refined is_validy for elliot algorithm

--- paul_elliot_algo/test_validity.py
+++ paul_elliot_algo/test_validity.py
@@ -1,18 +1,27 @@
-def is_conflict(sat, candidate):
+from src.propositional_state_logic import *
+from src.sat_solver import *
+from src.utils import *
+
+def is_conflict(sat: SATSolver, candidate: frozenset[Variable]):
     return not sat.check_consistency(candidate)[0]
 
-def get_all_children(d):
+def get_all_children(d: dict):
     for key, value in d.items():
         yield key
         if isinstance(value, dict):
             yield from get_all_children(value)
 
-def is_valid(candidate, sat, thruster_model_children):
-    # list of all the following partial assignments
-    childCandidateList = list(get_all_children(thruster_model_children))
-    for child in childCandidateList:
+def is_valid(candidate: frozenset[Variable], sat: SATSolver, model_children: dict):
+    validCandidateList = []
+    
+    for variable in candidate:
+        if len(variable.get_assignment()) == 1:
+            validCandidateList.append(variable)
+    
+    for child in list(frozenset(validCandidateList)):
         if is_conflict(sat, child):
             return False
+    
     return True
 
 def is_unsatisfiable(candidate, sat, thruster_model_children):
